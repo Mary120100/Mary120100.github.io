db699fc242113f95650a047cbc40916bb1e47b49
Mary120100 <39367671+Mary120100@users.noreply.github.com>
2018-11-03 21:52:40 +0200

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,32 @@
+﻿function $() 
+{ var elements = new Array(); 
+for (var i = 0; i < arguments.length; i++) 
+{ var element = arguments[i]; 
+if (typeof element == 'string') 
+element = document.getElementById(element); 
+if (arguments.length == 1) 
+return element; 
+elements.push(element); 
+} 
+return elements; }  
+ 
+function f1()
+{
+$('tx1').value="Математика";
+alert("Математика");
+}
+function f2()
+{
+$('tx1').value="Английский";
+alert("Английский");
+}
+function f3()
+{
+$('tx1').value="Физика";
+alert("Физика");
+}
+function f4()
+{
+$('tx1').value="Информатика";
+alert("Информатика");
+}
---
+++ scriptemail.js
@@ -0,0 +1,30 @@
+﻿﻿function $() 
+{ var elements = new Array(); 
+for (var i = 0; i < arguments.length; i++) 
+{ var element = arguments[i]; 
+if (typeof element == 'string') 
+element = document.getElementById(element); 
+if (arguments.length == 1) 
+return element; 
+elements.push(element); 
+} 
+return elements; }
+
+function f1(x)
+{
+var gg="abcdefghijklomnpqrstuvwxyz1234567890_.@";
+fff="Okay";
+var mass1 = x.split('@');
+if(mass1.length==2)
+{
+for(i=0;i<x.length;i++)
+{
+if(gg.indexOf(x.charAt(i))<0)
+fff="Fail";
+}
+}
+else
+fff="Fail";
+return fff;
+
+}
